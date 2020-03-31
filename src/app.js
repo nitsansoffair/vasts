dd60b90ba4ef345b293a6f52d4e6dfdaaf9f9b46
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-03-31 18:30:42 +0300

    dev.env

--- src/app.js
+++ src/app.js
@@ -5,4 +5,8 @@ require('./models/vasts');
 const db = require('./db/mysql');
 const app = express();
 
+db.execute('SELECT * FROM vasts')
+    .then(vasts => console.log(vasts))
+    .catch();
+
 module.exports = app;
