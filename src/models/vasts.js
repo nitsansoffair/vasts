6e524f4c2d4b840fc38b2af815d69a71c3cdd2f2
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-03-31 23:13:30 +0300

    Validations

--- src/models/vasts.js
+++ src/models/vasts.js
@@ -1,7 +1,6 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../db/mysql');
 
-// TODO - Add validators
 const Vast = sequelize.define('vasts', {
     id: {
         type: Sequelize.INTEGER,
@@ -10,19 +9,35 @@ const Vast = sequelize.define('vasts', {
         primaryKey: true
     },
     url: {
-        type: Sequelize.STRING(600)
+        type: Sequelize.STRING(600),
+        validate: {
+            isUrl: true
+        }
     },
     position: {
         type: Sequelize.STRING,
-        defaultValue: 'bottom_right'
+        defaultValue: 'bottom_right',
+        validate: {
+            isIn: [['top_left', 'top_middle', 'top_right', 'middle_left', 'middle_right', 'bottom_left', 'bottom_middle', 'bottom_right']]
+        }
     },
     width: {
         type: Sequelize.INTEGER,
-        defaultValue: 100
+        defaultValue: 100,
+        validate: {
+            isNumeric: true,
+            min: 100,
+            max: 1000
+        }
     },
     height: {
         type: Sequelize.INTEGER,
-        defaultValue: 100
+        defaultValue: 100,
+        validate: {
+            isNumeric: true,
+            min: 100,
+            max: 1000
+        }
     }
 });
 
