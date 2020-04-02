daa2219cc4eed754381da1b5a72b64757d6e438d
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-04-02 12:08:18 +0300

    Specify credentials for cors and migrate as env vars

--- server/src/app.js
+++ server/src/app.js
@@ -4,12 +4,10 @@ const vastRouter = require('./routes/vasts');
 
 const app = express();
 
-// TODO - Improve security
 app.use((req, res, next) => {
     res.set({
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
-        'Access-Control-Allow-Headers': 'Content-Type'
+        'Access-Control-Allow-Origin': process.env.ALLOWED_URL,
+        'Access-Control-Allow-Headers': process.env.ALLOWED_HEADERS
     });
     next();
 });
